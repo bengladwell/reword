155279a6abc20d08456d3565d2e7eafb9954b9a0
Ben Gladwell <bengladwell@gmail.com>
2015-09-18 08:55:20 -0400

    render list of words

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import AvailableWords from './components/AvailableWords';
 
+var words = [
+  {id: 0, text: 'hello'},
+  {id: 1, text: 'here'},
+  {id: 2, text: 'are'},
+  {id: 3, text: 'some'},
+  {id: 4, text: 'words'}
+];
+
 document.addEventListener('DOMContentLoaded', function () {
-  React.render(<AvailableWords />, document.getElementsByTagName('body')[0]);
+  React.render(<AvailableWords words={words} />, document.getElementsByTagName('body')[0]);
 });
--- src/js/components/AvailableWords.js
+++ src/js/components/AvailableWords.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 export default class AvailableWords extends Component {
   render() {
+    var words = this.props.words || [];
+
     return (
       <div>
         <h1>Available Words</h1>
+        <ul>
+          {words.map(function (word) {
+            return <li key={word.id}>{word.text}</li>;
+          })}
+        </ul>
       </div>
     );
   }
