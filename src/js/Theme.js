48ccb3bb05813e5af16677f3bbfe93f6b6506e0f
Ben Gladwell <bengladwell@gmail.com>
2015-10-12 09:43:00 -0400

    update style, add link to settings

--- src/js/Theme.js
+++ src/js/Theme.js
@@ -1,13 +1,15 @@
 import Colors from 'material-ui/lib/styles/colors';
 import LightTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
 
+// based on http://www.materialpalette.com/deep-orange/cyan
 export default Object.assign({}, LightTheme, {
   palette: Object.assign({}, LightTheme.palette, {
-    primary1Color: Colors.yellow300,
-    primary2Color: Colors.yellow700,
-    accent1Color: Colors.cyan200
-  }),
-  appBar: {
-    color: "#000"
-  }
+    primary1Color: Colors.deepOrange500,
+    primary2Color: Colors.deepOrange700,
+    primary3Color: Colors.deepOrange100,
+    accent1Color: Colors.cyan500,
+    textColor: Colors.grey900,
+    alternateTextColor: "#727272",
+    borderColor: "#b6b6b6"
+  })
 });
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import AppBar from 'material-ui/lib/app-bar';
+import FontIcon from 'material-ui/lib/font-icon';
 import Theme from '../Theme';
 
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
@@ -8,16 +10,26 @@ import Colors from 'material-ui/lib/styles/colors';
 export default class App extends Component {
   getChildContext() {
     var theme = ThemeManager.getMuiTheme(Theme);
-    theme.appBar.textColor = Colors.lightBlack;
+    theme.appBar.textColor = Colors.white;
     return {
       muiTheme: theme
     };
   }
 
   render() {
+    var theme = ThemeManager.getMuiTheme(Theme);
     return (
       <div className="app">
-        <AppBar title="reword" showMenuIconButton={false}/>
+        <AppBar
+          title="reword"
+          showMenuIconButton={false}
+          iconStyleRight={{marginTop: 0, marginRight: 0, lineHeight: theme.appBar.height + 'px', fontSize: 24}}
+          iconElementRight={
+            this.props.location.pathname === "/settings" ?
+                <Link to="/"><FontIcon className="material-icons" color={Colors.white}>arrow_back</FontIcon></Link>
+              : <Link to="/settings"><FontIcon className="material-icons" color={Colors.white}>settings</FontIcon></Link>
+          }
+        />
         {this.props.children}
       </div>
     );
